6924c4d0ff194b45ad263b3c3414401088310fcb
Yu-Jung Cheng <thisisyujungcheng@gmail.com>
2021-09-13 12:58:04 +1000

    fix build command

--- build-ubuntu-jenkins-agent-image.sh
+++ build-ubuntu-jenkins-agent-image.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-tag='ubuntu2004/jenkins_agent'
+if [ "${1}" == "" ]; then
+  tag='ubuntu2004/jenkins_agent'
+else
+  tag=${1}
+fi
 
-docker build -rm -t ${tag} .
+docker build --rm -t ${tag} .
